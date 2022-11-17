629e838b6cfff2561f1eb5acf5ecbc52978696e0
sandip2224 <sandipan2224@gmail.com>
2022-11-17 20:52:22 +0530

    Updated date with dynamic date value

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.get('/robinhood-apply', function (req, res) {
         data = data.replace(/\$OG_ORG/g, 'RobinHood')
         data = data.replace(/\$OG_URL/g, 'https://robinhood.com')
         data = data.replace(/\$OG_EMPLOYMENT/g, 'INTERNSHIP')
+        data = data.replace(/\$OG_ABOUTCOMPANY/g, `<p>The reliability of our platform takes precedence over all else, so that we can be there for our customers when they need us the most. We relentlessly protect our customers’ security and privacy, and we only share with our counterparties what they need to fulfill our customers’ financial needs, nothing more. We build safeguards and provide education so that our customers are in the best position to succeed. We have high quality timely customer support, and when things aren’t right, we fix them. We work closely with regulators and lawmakers to protect our customers and the broader financial system.</p><p>We never stop asking how we can make our product better, and we never settle for ‘good enough’. We listen to our colleagues, and we start from a place of believing they are capable and well-intentioned. We delight our customers and take pride in our work. Otherwise, why even be here?</p>`)
+        data = data.replace(/\$OG_DATE/g, new Date().toISOString())
         data = data.replace(/\$OG_TYPE/g, 'JobPosting')
         result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png')
         res.send(result)
